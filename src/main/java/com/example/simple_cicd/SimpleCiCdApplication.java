7f56a20a8d4da87810261d3afd2dfd8cca995651
Alexey Kirillov <alexey.kirillov@netcracker.com>
2021-10-05 12:19:42 +0300

    Added Swagger

--- src/main/java/com/example/simple_cicd/SimpleCiCdApplication.java
+++ src/main/java/com/example/simple_cicd/SimpleCiCdApplication.java
@@ -2,12 +2,28 @@ package com.example.simple_cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
+@EnableSwagger2WebMvc
 public class SimpleCiCdApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleCiCdApplication.class, args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 
 }

bcd906da617114f71b425969a8648a58de3237af
Alexey Kirillov <alexey.kirillov@netcracker.com>
2021-10-05 11:36:40 +0300

    Init

---
+++ src/main/java/com/example/simple_cicd/SimpleCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.example.simple_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCiCdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleCiCdApplication.class, args);
+    }
+
+}
